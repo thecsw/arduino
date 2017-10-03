b74971f0bc742bd8b8b29f7d0a4ecc5f652ba516
thecsw <thecsmoothiew@yahoo.com>
2017-10-03 16:29:40 +0400

    Add files via upload

---
+++ lm35/lm35.ino
@@ -0,0 +1,17 @@
+float tempC;
+int reading;
+int tempPin = 0;
+
+void setup()
+{
+Serial.begin(9600);
+}
+
+void loop()
+{
+reading = analogRead(tempPin);
+tempC = reading * 0.48828125;
+Serial.println(tempC);
+delay(1000);
+}
+
