b617272f9d3e8517d0f33fa90e49de0b09bd01e4
thecsw <thecsmoothiew@yahoo.com>
2017-10-03 16:28:58 +0400

    Add files via upload

---
+++ temp/temp.ino
@@ -0,0 +1,26 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire oneWire(2);
+DallasTemperature sensors(&oneWire);
+ 
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  sensors.begin();
+}
+ 
+ 
+void loop(void)
+{
+  Serial.print(" Requesting temperatures...");
+  sensors.requestTemperatures(); 
+  Serial.println("DONE");
+  Serial.print("Temperature for Device 1 is: ");
+  Serial.print(sensors.getTempCByIndex(0)); 
+  delay(500);
+
+}
+
